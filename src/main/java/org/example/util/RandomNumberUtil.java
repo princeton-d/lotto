dd02566a6ec82c039e648a17bd07dbf18474e0c6
websteak <ssk0080@gmail.com>
2023-03-24 15:33:33 +0900

    Random.nextInt() 적용, 로또 번호에 0이 나오던 부분 수정

--- src/main/java/org/example/util/RandomNumberUtil.java
+++ src/main/java/org/example/util/RandomNumberUtil.java
@@ -3,14 +3,16 @@ package org.example.util;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class RandomNumberUtil {
     private static final int MAX_LOTTO_NUMBER = 45;
     private static final int NUMBER_OF_LOTTO_NUMBER = 6;
-    
+    private static final Random random = new Random();
+
     public List<Integer> create() {
         List<Integer> randomNumberLotto = new ArrayList<>();
-        
+
         for (int i = 0; i < NUMBER_OF_LOTTO_NUMBER; i++) {
             int randomNumber = this.makeRandomNumber();
 
@@ -23,16 +25,15 @@ public class RandomNumberUtil {
             }
             Collections.sort(randomNumberLotto);
         }
-        
+
         return randomNumberLotto;
     }
-    
+
     private int makeRandomNumber() {
-        double number = Math.random() * MAX_LOTTO_NUMBER - 1;
-        int randomNumber = (int) Math.ceil(number);
+        int randomNumber = random.nextInt(MAX_LOTTO_NUMBER) + 1; // 1 ~ 45
         return randomNumber;
     }
-    
+
     private boolean isDuplicateNumber(List<Integer> list, int number) {
         return list.contains(number);
     }
